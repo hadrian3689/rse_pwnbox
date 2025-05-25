7a0d08177e9e622e4101958ce89b1934cc199d91
hadrian3689 <71423134+hadrian3689@users.noreply.github.com>
2025-05-25 16:06:20 -0400

    Update xfce4-panel-genmon-vpnip.sh

--- xfce4-panel-genmon-vpnip.sh
+++ xfce4-panel-genmon-vpnip.sh
@@ -2,12 +2,17 @@
 
 tun="$(ip a s tun0 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
 eth="$(ip a s eth0 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
+eth1="$(ip a s eth1 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
 lo="$(ip a s lo 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
 
 if [ "$tun" != "" ]; then
   printf "<icon>network-vpn-symbolic</icon>"
   printf "<txt> VPN ${tun} </txt>"
   printf "<tool>VPN IP</tool>"
+elif [ "$eth" != "" ] && [ "$eth1" != "" ]; then
+  printf "<icon>network-vpn-symbolic</icon>"
+  printf "<txt> IP ${eth} | ${eth1} </txt>"
+  printf "<tool>My IP</tool>"
 elif [ "$eth" != "" ]; then
   printf "<icon>network-vpn-symbolic</icon>"
   printf "<txt> IP ${eth} </txt>"
